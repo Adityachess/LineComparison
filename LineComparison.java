c63c807b3110bcbf3e9597baabe251cd65c93c61
Adityachess <adityadone9398@gmail.com>
2021-09-20 16:13:29 +0530

    Welcome message

---
+++ LineComparison.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.Linecomapre;
+
+public class LineComparison {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Lone Comparison computation {Program");
+
+	}
+
+}
