deb7b17c32ee899f21786f76a4a0a43cf2e7cc6a
Adityachess <adityadone9398@gmail.com>
2021-09-20 16:16:50 +0530

    Calculating length 1

--- LineComparison.java
+++ LineComparison.java
@@ -1,11 +1,27 @@
 package com.bridgelabz.Linecomapre;
 
+import java.util.Scanner;
+
 public class LineComparison {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Lone Comparison computation {Program");
+		
+	Scanner scan = new Scanner(System.in);
+		
+		System.out.println("enter the values:");
+		
+		double x1 = scan.nextDouble();
+		double x2 = scan.nextDouble();
+		double y1 = scan.nextDouble();
+		double y2 = scan.nextDouble();
+		
+		double len1 = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+		
+		System.out.println("length is:" + len1);
+	}
 
 	}
 
-}
+
